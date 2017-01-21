83e26751541b1f94641be4c432d01fb0824442a2
Rémy Bouteloup <remy.bouteloup@yahoo.fr>
2017-01-21 18:41:19 +0100

    Amélioration du code dans exec_command. Optimisation de 42 lignes sur la redirection

--- code/shell_fct.c
+++ code/shell_fct.c
@@ -25,13 +25,78 @@ void alarmHandler()
 	kill(pid, SIGKILL);
 }
 
+// Redirection
+int redirection(cmd * my_cmd, int ** tube, int i)
+{
+	// Permission du fichier créé // Pour STDOUT ET STDERR
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	int file;
+
+	// Si redirection <
+	if (my_cmd->redirection[i][STDIN] != NULL)
+	{
+		file = open(my_cmd->redirection[i][STDIN], O_RDONLY);
+
+		if (file < 0)
+		{
+			printf("Redirection : Echec de l'ouverture du fichier");
+			return -1;
+		}
+
+		dup2(file, 0);
+
+		if (my_cmd->nb_cmd_members > i + 1)
+		{
+			close(tube[i][0]);
+			dup2(tube[i][1], 1);
+			close(tube[i][1]);
+		}
+		return true;
+	}
+
+	// Si redirection > ou >>
+	if (my_cmd->redirection[i][STDOUT] != NULL)
+	{
+		if (my_cmd->redirection_type[i][STDOUT] == OVERRIDE)
+			file = open(my_cmd->redirection[i][STDOUT], O_WRONLY | O_TRUNC | O_CREAT, mode);
+		else
+			file = open(my_cmd->redirection[i][STDOUT], O_WRONLY | O_APPEND | O_CREAT, mode);
+	}
+	else // Si redirection 2> ou 2>>
+	{
+		if (my_cmd->redirection_type[i][STDERR] == OVERRIDE)
+			file = open(my_cmd->redirection[i][STDERR], O_WRONLY | O_TRUNC | O_CREAT, mode);
+		else
+			file = open(my_cmd->redirection[i][STDERR], O_WRONLY | O_APPEND | O_CREAT, mode);
+	}
+
+	// Test de l'ouverture du fichier
+	if (file < 0)
+	{
+		printf("Redirection : Echec de l'ouverture du fichier");
+		return -1;
+	}
+
+	// STDOUT
+	dup2(file, 1);
+
+	// Pas de données en input si je suis au premier membre
+	if (i > 0)
+	{
+		close(tube[i-1][1]);
+		dup2(tube[i-1][0], 0);
+		close(tube[i-1][0]);
+	}
+	return file;
+}
+
+
 // Création des pipes et processus fils pour l'execution de la commande
 int exec_command(cmd * my_cmd)
 {
 	// Variable
-	int i = 0, status, file;
+	int i = 0, file = 0, status; 
 	int ** tube = NULL;
-	bool redirect = false;
 
 	// Retourne -1 si la commande est vide
 	if (my_cmd->nb_cmd_members == 0)
@@ -77,118 +142,11 @@ int exec_command(cmd * my_cmd)
 		pid = fork();
 		if(pid == 0) // Traitement du processus fils
 		{
-			// Si redirection <
-			if (my_cmd->redirection[i][STDIN] != NULL)
+			if (my_cmd->redirection[i][STDIN] != NULL || my_cmd->redirection[i][STDOUT] != NULL || my_cmd->redirection[i][STDERR] != NULL)
 			{
-				redirect = true;
-
-				file = open(my_cmd->redirection[i][STDIN], O_RDONLY);
-
-				if (file < 0)
-				{
-					printf("Echec de l'ouverture du fichier %s",my_cmd->redirection[i][STDIN]);
-					return -1;
-				}
-				dup2(file, 0);
-
-				if (my_cmd->nb_cmd_members > i + 1)
-				{
-					close(tube[i][0]);
-					dup2(tube[i][1], 1);
-					close(tube[i][1]);
-				}
+				file = redirection(my_cmd, tube, i);
 			}
-			else if (my_cmd->redirection[i][STDOUT] != NULL)
-			{
-				redirect = true;
-
-				if (my_cmd->redirection_type[i][STDOUT] == OVERRIDE)
-				{
-					mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-					file = open(my_cmd->redirection[i][STDOUT], O_WRONLY | O_TRUNC | O_CREAT, mode);
-
-					if (file < 0)
-					{
-						printf("Echec de l'ouverture du fichier %s",my_cmd->redirection[i][STDOUT]);
-						return -1;
-					}
-					dup2(file, 1);
-
-					// Pas de données en input si je suis au premier membre parmis
-					if (i > 0)
-					{
-						close(tube[i-1][1]);
-						dup2(tube[i-1][0], 0);
-						close(tube[i-1][0]);
-					}
-				}
-				else if (my_cmd->redirection_type[i][STDOUT] == APPEND)
-				{
-					mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-					file = open(my_cmd->redirection[i][STDOUT], O_WRONLY | O_APPEND | O_CREAT, mode);
-
-					if (file < 0)
-					{
-						printf("Echec de l'ouverture du fichier %s",my_cmd->redirection[i][STDOUT]);
-						return -1;
-					}
-					dup2(file, 1);
-					
-					// Pas de données en input si je suis au premier membre parmis
-					if (i > 0)
-					{
-						close(tube[i-1][1]);
-						dup2(tube[i-1][0], 0);
-						close(tube[i-1][0]);
-					}
-				}
-			}
-			else if (my_cmd->redirection[i][STDERR] != NULL)
-			{
-				redirect = true;
-
-				if (my_cmd->redirection_type[i][STDERR] == OVERRIDE)
-				{
-					mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-					file = open(my_cmd->redirection[i][STDERR], O_WRONLY | O_TRUNC | O_CREAT, mode);
-
-					if (file < 0)
-					{
-						printf("Echec de l'ouverture du fichier %s",my_cmd->redirection[i][STDERR]);
-						return -1;
-					}
-					dup2(file, 2);
-
-					// Pas de données en input si je suis au premier membre parmis
-					if (i > 0)
-					{
-						close(tube[i-1][1]);
-						dup2(tube[i-1][0], 0);
-						close(tube[i-1][0]);
-					}
-				}
-				else if (my_cmd->redirection_type[i][STDERR] == APPEND)
-				{
-					mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-					file = open(my_cmd->redirection[i][STDERR], O_WRONLY | O_APPEND | O_CREAT, mode);
-
-					if (file < 0)
-					{
-						printf("Echec de l'ouverture du fichier %s",my_cmd->redirection[i][STDERR]);
-						return -1;
-					}
-					dup2(file, 2);
-
-					// Pas de données en input si je suis au premier membre parmis
-					if (i > 0)
-					{
-						close(tube[i-1][1]);
-						dup2(tube[i-1][0], 0);
-						close(tube[i-1][0]);
-					}
-				}
-			}
-			else if (my_cmd->nb_cmd_members > 1 && redirect == false) // Inutile d'utiliser des tubes si un seul membre dans la commande
+			else if (my_cmd->nb_cmd_members > 1) // Inutile d'utiliser des tubes si un seul membre dans la commande
 			{
 				// Pas de données en input si je suis au premier membre parmis
 				if (i > 0)
@@ -214,11 +172,11 @@ int exec_command(cmd * my_cmd)
 				if((execvp(my_cmd->cmd_members_args[i][0], my_cmd->cmd_members_args[i])) == -1)
 				{
 					printf("Commande incorrecte !!\n");
-					return -1;
+					return -2;
 				}
 			}
 
-			if (redirect == true)
+			if (file > 0)
 				close(file);
 
 			// Valeur de retour du processus fils
@@ -227,7 +185,6 @@ int exec_command(cmd * my_cmd)
 
 		// Incrémentation de i : Passage au membre suivant
 		i++;
-		redirect = false;
 
 		// Fermeture des tubes au fur et à mesure de leur création
 		// Ils sont fermés après leur utilisation
--- code/shell_fct.h
+++ code/shell_fct.h
@@ -6,5 +6,12 @@
 
 //Execute a command
 int exec_command(cmd* my_cmd);
+
+// Traitement pour effectuer les redirections
+int redirection(cmd * my_cmd, int ** tube, int i);
+
+// Chien de garde des processus
 void alarmHandler();
+
+// Libération de la mémoire mise sur le tas
 void destroy(cmd * c, int returnValue);
